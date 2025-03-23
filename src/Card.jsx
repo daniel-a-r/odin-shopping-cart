008edeb23bf5962e987ea5244dae4b237ea5f65d
daniel-a-r <daniel.a-r@outlook.com>
2025-03-23 01:55:56 -0400

    add styling

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,9 +4,10 @@ const Card = ({ imageURL, price, title }) => {
   return (
     <li className="card">
       <img className="card-img" src={imageURL} alt='' />
+      <div className='card-info'>
       <p>{title}</p>
-      <p>${price}</p>
-      {/* <p>Hello</p> */}
+      <p className='card-price'>${price}</p>
+      </div>
     </li>
   );
 };
